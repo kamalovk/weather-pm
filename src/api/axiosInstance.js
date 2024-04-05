6b6ec3110229873da6f72991c7864be3d19999bb
46kvolamak <you@example.com>
2024-04-05 10:34:17 +0500

    axiosInstance: fix https url

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -10,7 +10,7 @@ export const axiosInstance = axios.create({
 });
 
 export const geoInstance = axios.create({
-  baseURL: 'http://api.openweathermap.org/geo/1.0/',
+  baseURL: 'https://api.openweathermap.org/geo/1.0/',
   params: {
     appid: '0d20b49ce8d6817eb0aff8b3bb09cd5e',
     units: 'metric'
